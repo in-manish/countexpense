3ac54d2e3bdb4ad98e30d3e13b288b630de29b1f
Manish <memanishindian@gmail.com>
2018-08-02 12:33:26 +0530

    added widget datetime picker

--- countexpense/settings.py
+++ countexpense/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'expense',
     'widget_tweaks',
+    'datetimepicker',
 ]
 
 MIDDLEWARE = [
--- expense/forms.py
+++ expense/forms.py
@@ -1,9 +1,10 @@
 from django import forms
+from datetimepicker.widgets import DateTimePicker
 
 from .models import ExpenseModel
 class AddNewExpenseForm(forms.ModelForm):
 	description = forms.CharField(widget=forms.Textarea())
-	expense_time = forms.DateField(widget=forms.DateInput())
+	expense_time = forms.DateTimeField(widget=forms.widgets.DateInput(attrs={'type': 'date'}))
 	class Meta:
 		model = ExpenseModel
 		fields = ['payment_method', 'description', 'amount','share', 'expense_time', 'category' ]
